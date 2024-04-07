10b476dca7b05075d7602a9f1ade91b69b9110e7
Matoma99 <166150676+Matoma99@users.noreply.github.com>
2024-04-07 02:56:44 -0500

    ,.

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Administrador de Materias y Estudiantes</title>
-  <!-- Incluir CSS de Bootstrap -->
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
@@ -59,8 +58,6 @@
     <button type="submit" class="btn btn-primary">Crear Estudiante</button>
   </form>
 </div>
-
-<!-- Incluir JS de Bootstrap y jQuery (opcional) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
